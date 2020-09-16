0d42841f0c1f411b96c29d7f2c53ddc3b2e5a50f
apple502j <33279053+apple502j@users.noreply.github.com>
2020-09-16 22:05:43 +0900

    SpecialRequestAccount i18n

--- src/SpecialRequestAccount.php
+++ src/SpecialRequestAccount.php
@@ -8,153 +8,206 @@ class SpecialRequestAccount extends SpecialPage {
 	function __construct() {
 		parent::__construct( 'RequestAccount' );
 	}
-	
+
 	function sanitizedPostData(&$request, &$session, &$out_error) {
 		$username = $request->getText('scratchusername');
-		
+
 		if ($username == '' || !isValidScratchUsername($username)) {
-			$out_error = 'invalid scratch username';
+			$out_error = wfMessage('scratch-confirmaccount-invalid-username')->text();
 			return;
 		}
-		
+
 		if (userExists($username)) {
-			$out_error = 'user already exists';
+			$out_error = wfMessage('scratch-confirmaccount-user-exists')->text();
 			return;
 		}
-		
+
 		if (hasActiveRequest($username)) {
-			$out_error = 'user already has active request';
+			$out_error = wfMessage('scratch-confirmaccount-request-exists')->text();
 			return;
 		}
-		
+
 		if (topVerifCommenter(sessionVerificationCode($session)) != $username) {
-			$out_error = 'verification code missing';
+			$out_error = wfMessage('scratch-confirmaccount-verif-missing', $username)->text();
 			return;
 		}
-		
+
 		$blockReason = getBlockReason($username);
 		if ($blockReason) {
-			$out_error = 'username blocked with reason: ' . $blockReason; // note : blocks are not publicly visible on scratch, so this needs to run after checking the verification code
+			$out_error = wfMessage('scratch-confirmaccount-user-blocked', $blockReason)->text();
+			// note : blocks are not publicly visible on scratch, so this needs to run after checking the verification code
 			return;
 		}
-		
+
 		$email = $request->getText('email');
 		if ($email != '' && !Sanitizer::validateEmail($email)) {
-			$out_error = 'invalid email';
+			$out_error = wfMessage('scratch-confirmaccount-invalid-email')->text();
 			return;
 		}
-		
-		if($request->getText('agree') != "true"){
-			$out_error = "did not agree to checkbox";
-			return;
-		}
-		
-		$request_notes = $request->getText('requestnotes');
-		
-		if($request_notes == ""){
-			$out_error = "no request notes";
-			return;
-		}
-		
 
-		
+		if($request->getText('agree') != "true"){
+			$out_error = wfMessage('scratch-confirmaccount-disagree-tos')->text();
+			return;
+		}
+
+		$request_notes = $request->getText('requestnotes');
+
+		if($request_notes == ""){
+			$out_error = wfMessage('scratch-confirmaccount-no-request-notes')->text();
+			return;
+		}
+
+
+
 		return ['username' => $username, 'email' => $email, 'requestnotes' => $request_notes];
 	}
-	
+
 	function getGroupName() {
 		return 'login';
 	}
-	
+
 	function formSectionHeader($name) {
-		$form = Xml::openElement('fieldset');
-		$form .= Xml::openElement('legend');
+		$form = Html::openElement('fieldset');
+		$form .= Html::openElement('legend');
 		$form .= $name;
-		$form .= Xml::closeElement('legend');
+		$form .= Html::closeElement('legend');
 
 		return $form;
 	}
-	
+
 	function formSectionFooter() {
-		return Xml::closeElement('fieldset');
+		return Html::closeElement('fieldset');
 	}
-	
+
 	function usernameAndVerificationArea(&$session, $request) {
-		$form = $this->formSectionHeader(wfMessage('scratch-confirmaccount-usernameverification'));
-		
-		$form .= '<p>';
-		$form .= '<label for="scratch-confirmaccount-username">' . wfMessage('scratch-confirmaccount-scratchusername') . '</label><br />';
-		$form .= Html::rawElement('input', ['type' => 'text', 'name' => 'scratchusername', 'id' => 'scratch-confirmaccount-username', 'value' => $request->getText('scratchusername')]);
-		$form .= '</p>';
-		
-		$form .= '<p>';
-		$form .= '<label for="scratch-confirmaccount-email">' . wfMessage('scratch-confirmaccount-email') . '</label><br />';
-		$form .= Html::rawElement('input', ['type' => 'email', 'name' => 'email', 'id' => 'scratch-confirmaccount-email', 'value' => $request->getText('email')]);
-		$form .= '</p>';
-		
-		$form .= '<p>' . wfMessage('scratch-confirmaccount-vercode-explanation')->params(sprintf(PROJECT_LINK, wgScratchVerificationProjectID()))->parse() . '</p>';
-		$form .= '<p style=\"font-weight: bold\">' . sessionVerificationCode($session) . '</p>';
-		
+		$form = $this->formSectionHeader(wfMessage('scratch-confirmaccount-usernameverification')->text());
+
+		$form .= Html::openElement('p');
+		$form .= Html::element(
+			'label',
+			['for' => 'scratch-confirmaccount-username'],
+			wfMessage('scratch-confirmaccount-scratchusername')->text()
+		);
+		$form .= Html::element(
+			'input',
+			[
+				'type' => 'text',
+				'name' => 'scratchusername',
+				'id' => 'scratch-confirmaccount-username',
+				'value' => $request->getText('scratchusername')
+			]
+		);
+		$form .= Html::closeElement('p');
+
+		$form .= Html::openElement('p');
+		$form .= Html::element(
+			'label',
+			['for' => 'scratch-confirmaccount-email'],
+			wfMessage('scratch-confirmaccount-email')->text()
+		);
+		$form .= Html::element(
+			'input',
+			[
+				'type' => 'email',
+				'name' => 'email',
+				'id' => 'scratch-confirmaccount-email',
+				'value' => $request->getText('email')
+			]
+		);
+		$form .= Html::closeElement('p');
+
+		$form .= Html::rawElement(
+			'p',
+			[],
+			wfMessage('scratch-confirmaccount-vercode-explanation', sprintf(PROJECT_LINK, wgScratchVerificationProjectID()))->parse()
+		);
+		$form .= Html.element(
+			'p',
+			['class' => 'mw-scratch-confirmaccount-verifcode'],
+			sessionVerificationCode($session)
+		);
+
 		$form .= $this->formSectionFooter();
-		
+
 		return $form;
 	}
-	
+
 	function requestNotesArea(&$request) {
-		$form = $this->formSectionHeader(wfMessage('scratch-confirmaccount-requestnotes'));
-		
-		$form .= '<p>' . wfMessage('scratch-confirmaccount-requestnotes-explanation')->parse() . '</p>';
-		
-		$form .= '<label for="scratch-confirmaccount-requestnotes">' . wfMessage('scratch-confirmaccount-requestnotes') . '</label>';
+		$form = $this->formSectionHeader(wfMessage('scratch-confirmaccount-requestnotes')->text());
+
+		$form .= Html::element('p', [], wfMessage('scratch-confirmaccount-requestnotes-explanation')->parse());
+		$form .= Html::element(
+			'label',
+			['for' => 'scratch-confirmaccount-requestnotes'],
+			wfMessage('scratch-confirmaccount-requestnotes')->text()
+		);
 		$form .= Html::element('textarea', ['id' => 'scratch-confirmaccount-requestnotes', 'name' => 'requestnotes'], $request->getText('requestnotes'));
-		
-		$form .= '<br>';
 
-		$form .= Html::element('input', ['type' => 'checkbox', 'name' => 'agree', 'value' => 'true']);
+		$form .= Html::openElement('br');
 
-		$form .= ' ' . wfMessage('scratch-confirmaccount-checkbox-agree') . '<br>';
-		
+		$form .= Html::element('input', [
+			'type' => 'checkbox',
+			'name' => 'agree',
+			'value' => 'true',
+			'id' => 'scratch-confirmaccount-agree'
+		]);
+		$form .= Html::element(
+			'label',
+			['for' => 'scratch-confirmaccount-agree'],
+			wfMessage('scratch-confirmaccount-checkbox-agree')->text()
+		);
+		$form .= Html::openElement('br');
+
 		$form .= $this->formSectionFooter();
-		
+
 		return $form;
 	}
-	
+
 	function guidelinesArea() {
-		return '';
+		return wfMessage('scratch-confirmaccount-guideline')->parse();
 	}
-	
+
 	function handleFormSubmission(&$request, &$output, &$session) {
 		//validate and sanitize the input
 		$formData = $this->sanitizedPostData($request, $session, $error);
 		if ($error != '') {
 			return $this->requestForm($request, $output, $session, $error);
 		}
-		
+
 		//now actually create the request and reset the verification code
 		createAccountRequest($formData['username'], $formData['requestnotes'], $formData['email'], $request->getIP());
 		generateNewCodeForSession($session);
-		
+
 		//and show the output
-		$output->addHTML('success');
+		$output->addHTML(Html::element(
+			'p',
+			[],
+			wfMessage('scratch-confirmaccount-success')->text()
+		));
 	}
-	
+
 	function requestForm(&$request, &$output, &$session, $error = '') {
-		$form = Xml::openElement('form', [ 'method' => 'post', 'name' => 'requestaccount', 'action' => $this->getPageTitle()->getLocalUrl(), 'enctype' => 'multipart/form-data' ]);
-		
+		$form = Html::openElement('form', [ 'method' => 'post', 'name' => 'requestaccount', 'action' => $this->getPageTitle()->getLocalUrl(), 'enctype' => 'multipart/form-data' ]);
+
 		//display errors if there are any relevant
-		//TODO: show this in an error box
 		if ($error != '') {
-			$form .= '<p>' . $error . '</p>';
+			$form .= Html::element('p', ['class' => 'error'], $error);
 		}
-		
+
 		//form body
 		$form .= $this->usernameAndVerificationArea($session, $request);
 		$form .= $this->requestNotesArea($request);
 		$form .= $this->guidelinesArea();
-		
-		$form .= '<input type="submit" value="' . wfMessage('scratch-confirmaccount-request-submit') . '" />';
-		
-		$form .= Xml::closeElement('form');
-		
+
+		$form .= Html::element('input',
+			[
+				'type' => 'submit',
+				'value' => wfMessage('scratch-confirmaccount-request-submit')->parse()
+			]
+		);
+
+		$form .= Html::closeElement('form');
+
 		$output->addHTML($form);
 	}
 
@@ -163,7 +216,7 @@ class SpecialRequestAccount extends SpecialPage {
 		$output = $this->getOutput();
 		$session = $this->getRequest()->getSession();
 		$this->setHeaders();
-		
+
 		if ($request->wasPosted()) {
 			return $this->handleFormSubmission($request, $output, $session);
 		} else {
