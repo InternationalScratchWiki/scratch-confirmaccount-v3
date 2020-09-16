fe27c458b34c8fc4584c52573c9701662b6d4a89
apple502j <33279053+apple502j@users.noreply.github.com>
2020-09-17 04:18:58 +0900

    Store password hash and use it when creating

--- src/database/DatabaseInteractions.php
+++ src/database/DatabaseInteractions.php
@@ -8,17 +8,18 @@ function getBlockReason($username) {
 	return $row ? $row->block_reason : false;
 }
 
-function createAccountRequest($username, $requestNotes, $email, $ip) {
+function createAccountRequest($username, $passwordHash, $requestNotes, $email, $ip) {
 	$dbw = wfGetDB( DB_MASTER );
 	$dbw->insert('scratch_accountrequest', [
 		'request_username' => $username,
+		'password_hash' => $passwordHash,
 		'request_email' => $email,
 		'request_timestamp' => wfTimestampNow(),
 		'request_notes' => $requestNotes,
 		'request_ip' => $ip,
 		'request_status' => 'new'
 	], __METHOD__);
-	
+
 	return $dbw->insertID();
 }
 
@@ -30,28 +31,28 @@ function getAccountRequests(string $status = null, string $username = null, int 
 	if ($username != null) {
 		$criteria['LOWER(request_username)'] = strtolower($username);
 	}
-	
+
 	$dbr = wfGetDB( DB_REPLICA );
-	$result = $dbr->select('scratch_accountrequest', array('request_id', 'request_username', 'request_email', 'request_timestamp', 'request_notes', 'request_ip', 'request_status'), $criteria, __METHOD__, ['order_by' => ['request_timestamp', 'DESC']]);
-	
+	$result = $dbr->select('scratch_accountrequest', array('request_id', 'request_username', 'password_hash', 'request_email', 'request_timestamp', 'request_notes', 'request_ip', 'request_status'), $criteria, __METHOD__, ['order_by' => ['request_timestamp', 'DESC']]);
+
 	$requests = array();
 	foreach ($result as $row) {
 		$requests[] = AccountRequest::fromRow($row);
 	}
-	
+
 	return $requests;
 }
 
 function getAccountRequestById($id) {
 	$dbr = wfGetDB( DB_REPLICA );
-	$result = $dbr->selectRow('scratch_accountrequest', array('request_id', 'request_username', 'request_email', 'request_timestamp', 'request_notes', 'request_ip', 'request_status'), ['request_id' => $id], __METHOD__);
-	
+	$result = $dbr->selectRow('scratch_accountrequest', array('request_id', 'request_username', 'password_hash', 'request_email', 'request_timestamp', 'request_notes', 'request_ip', 'request_status'), ['request_id' => $id], __METHOD__);
+
 	return $result ? AccountRequest::fromRow($result) : false;
 }
 
 function actionRequest(AccountRequest $request, string $action, $userPerformingAction, string $comment) {
 	$dbw = wfGetDB( DB_MASTER );
-	
+
 	$dbw->insert('scratch_accountrequest_history', [
 		'history_request_id' => $request->id,
 		'history_action' => $action,
@@ -59,7 +60,7 @@ function actionRequest(AccountRequest $request, string $action, $userPerformingA
 		'history_performer' => $userPerformingAction,
 		'history_timestamp' => wfTimestampNow()
 	], __METHOD__);
-	
+
 	//if the action also updates the status, then set the status appropriately
 	if (isset(actionToStatus[$action])) {
 		$dbw->update('scratch_accountrequest', [
@@ -70,7 +71,7 @@ function actionRequest(AccountRequest $request, string $action, $userPerformingA
 
 function getRequestHistory(AccountRequest $request) : array {
 	$dbr = wfGetDB( DB_REPLICA );
-	
+
 	$result = $dbr->select(['scratch_accountrequest_history', 'user'], [
 		'history_timestamp',
 		'history_action',
@@ -79,27 +80,37 @@ function getRequestHistory(AccountRequest $request) : array {
 	], ['history_request_id' => $request->id], __METHOD__, ['order_by' => ['history_timestamp', 'ASC']], [
 		'user' => ['LEFT JOIN', ['user_id=history_performer']]
 	]);
-	
+
 	$history = array();
 	foreach ($result as $row) {
 		$history[] = AccountRequestHistoryEntry::fromRow($row);
 	}
-	
+
 	return $history;
 }
 
 function createAccount(AccountRequest $request) {
-	User::newFromName($request->username)->addToDatabase();
+	$user = User::newFromName($request->username);
+	$user->addToDatabase();
+	$dbw = wfGetDB( DB_MASTER );
+	$dbw->update(
+		'user',
+		[
+			'user_password' => $request->passwordHash
+		],
+		[ 'user_id' => $user->getId() ],
+		__METHOD__
+	);
 }
 
 function userExists(string $username) : bool {
 	$dbr = wfGetDB( DB_REPLICA );
-	
+
 	return User::newFromName($username)->getId() != 0; //TODO: make this case-insensitive
 }
 
 function hasActiveRequest(string $username) : bool {
 	$dbr = wfGetDB( DB_REPLICA );
-	
+
 	return $dbr->selectRowCount('scratch_accountrequest', array('1'), ['LOWER(request_username)' => strtolower($username), 'request_status' => ['new', 'awaiting-admin', 'awaiting-user']], __METHOD__) > 0;
 }
