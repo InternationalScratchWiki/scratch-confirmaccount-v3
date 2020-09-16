1d05877cb385dcdd110ddc19eb183f480973fa75
Jacob G <jacob-x@jvvgindustries.com>
2020-09-16 01:27:43 +0000

    added reason field to blocks table

--- sql/blocks.sql
+++ sql/blocks.sql
@@ -5,6 +5,8 @@ CREATE TABLE IF NOT EXISTS /*_*/scratch_accountrequest_block (
 	block_username VARCHAR(255) binary NOT NULL PRIMARY KEY,
 	-- user.user_id of the user who performed the block
 	block_blocker_user_id integer unsigned NOT NULL,
+	-- the reason the user was blocked
+	block_reason TEXT,
 	-- timestamp for when the block was performed
 	block_timestamp VARBINARY(14) NOT NULL
 )/*$wgDBTableOptions*/;
