ffc123d7159ad9e268fa647b429eaf50b0d30c83
Jacob G <jacob-x@jvvgindustries.com>
2020-09-17 17:43:09 +0000

    on second thought, request expiry should be allowed to be null

--- sql/requests.sql
+++ sql/requests.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS /*_*/scratch_accountrequest_request (
 	request_email VARCHAR(255) binary,
 	request_timestamp varbinary(14) NOT NULL,
 	request_last_updated varbinary(14) NOT NULL,
-	request_expiry varbinary(14) NOT NULL,
+	request_expiry varbinary(14),
 	request_notes TEXT NOT NULL,
 	request_ip VARCHAR(255) NOT NULL,
 	request_status VARCHAR(255) binary NOT NULL DEFAULT 'new'
