dd79a05a815ed20aac08705f4ea6793916dc13e8
R4356th <61620631+R4356th@users.noreply.github.com>
2021-01-25 06:13:27 +0600

    Make special pages translatable (#103)
    * Create ScratchConfirmAccount.i18n.alias.php
    
    * Add alias to extension.json

---
+++ src/ScratchConfirmAccount.i18n.alias.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Special page aliases for localisation
+ */
+
+$specialPageAliases = [];
+
+// English
+$specialPageAliases['en'] = [
+  'RequestAccount' => [ 'RequestAccount' ],
+  'ConfirmAccounts' => [ 'ConfirmAccounts' ],
+];
