426f819c824889e0f1aa13edeaefc1f04457c955
Jacob G <jacob-x@jvvgindustries.com>
2021-09-27 18:04:44 +0100

    added database table

---
+++ sql/requirements_bypass.sql
@@ -0,0 +1,8 @@
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS /*_*/scratch_accountrequest_requirements_bypass (
+	-- Primary key
+	bypass_username VARCHAR(255) binary NOT NULL PRIMARY KEY,
+)/*$wgDBTableOptions*/;
+
+COMMIT;
